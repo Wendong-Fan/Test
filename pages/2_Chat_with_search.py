9541faeabb691d326c895534ccd83a2929553206
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2023-08-04 16:25:58 +0800

    Update 2_Chat_with_search.py

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -2,39 +2,52 @@ import streamlit as st
 
 from langchain.agents import initialize_agent, AgentType
 from langchain.callbacks import StreamlitCallbackHandler
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import AzureChatOpenAI
 from langchain.tools import DuckDuckGoSearchRun
+from PIL import Image
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+key1 = st.secrets["key1"]
 
-st.title("🔎 LangChain - Chat with search")
+st.title("🧐 小佛陀")
+#image0 = Image.open('images.jpeg')
+#st.image(image0)
 
-"""
-In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
-Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
-"""
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hi, I'm a chatbot who can search the web. How can I help you?"}
+        {"role": "assistant", "content": "我是一股无形的智慧之力，能洞察世间万物，解答你心中的疑惑。我在你的思考中存在，引导你走向真理与和平。"}
     ]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+if prompt := st.chat_input(placeholder="宇宙的起源是什么"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+    llm = AzureChatOpenAI(
+                        openai_api_base = "https://azureopenai-mutiagent.openai.azure.com/",
+                        openai_api_version = "2023-03-15-preview",
+                        openai_api_key = key1,
+                        openai_api_type = "azure",
+                        deployment_name="gpt-35-turbo",
+                        model_name="gpt-35-turbo", 
+                        streaming=True)
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
+    search = DuckDuckGoSearchRun(name="Search")
+    search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)reopenai-mutiagent.openai.azure.com/",
+                        openai_api_version = "2023-03-15-preview",
+                        openai_api_key = key1,
+                        openai_api_type = "azure",
+                        deployment_name="gpt-35-turbo",
+                        model_name="gpt-35-turbo", 
+                        streaming=True)
+
     search = DuckDuckGoSearchRun(name="Search")
     search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
     with st.chat_message("assistant"):
